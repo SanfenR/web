47d82302caf40d9d78180ec89d248826f1e293cd
sanfen <sanfen@sanfendeMacBook-Pro.local>
2016-05-09 17:09:10 +0800

    修改界面

---
+++ my/src/server/Servlet.java
@@ -0,0 +1,21 @@
+package server;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sanfen on 16/5/9.
+ */
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+}
